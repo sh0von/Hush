66bbab5850e10262aa50a2a08ba6d5657e525d19
sh0von <minar.svn@gmail.com>
2024-03-20 15:34:52 +0600

    Emnei

--- backend/app.js
+++ backend/app.js
@@ -9,10 +9,11 @@ app.use(cors());
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect(process.env.MONGODB, {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 db.once("open", () => {
